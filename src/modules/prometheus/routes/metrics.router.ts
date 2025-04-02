95d530eba16cc08a8268c26cf5c02474c2850854
Florian Duport <florianduport@mac.lan>
2025-04-02 17:04:10 +0200

    upgrade endurance core

--- src/modules/prometheus/routes/metrics.router.ts
+++ src/modules/prometheus/routes/metrics.router.ts
@@ -1,39 +1,69 @@
 import client from 'prom-client';
-import { createRouter } from 'endurance-core';
-const router = createRouter();
-const isPrometheusActivated = process.env.PROMETHEUS_ACTIVATED !== 'false';
+import { EnduranceRouter, type SecurityOptions } from 'endurance-core';
 
-if (isPrometheusActivated) {
-  const register = new client.Registry();
+class PrometheusRouter extends EnduranceRouter {
+  private register: any;
+  private httpRequestDurationSeconds: any;
+  private httpRequestErrorsTotal: any;
 
-  client.collectDefaultMetrics({ register });
+  constructor() {
+    super();
+    this.initializeMetrics();
+  }
 
-  const httpRequestDurationSeconds = new client.Histogram({
-    name: 'http_request_duration_seconds',
-    help: 'Duration of HTTP requests in seconds',
-    labelNames: ['method', 'route', 'status_code'],
-    buckets: [0.1, 0.5, 1, 1.5, 2, 5]
-  });
+  private initializeMetrics(): void {
+    const isPrometheusActivated = process.env.PROMETHEUS_ACTIVATED !== 'false';
 
-  const httpRequestErrorsTotal = new client.Counter({
-    name: 'http_request_errors_total',
-    help: 'Total number of HTTP request errors',
-    labelNames: ['method', 'route', 'status_code']
-  });
+    if (isPrometheusActivated) {
+      this.register = new client.Registry();
+      client.collectDefaultMetrics({ register: this.register });
 
-  register.registerMetric(httpRequestDurationSeconds);
-  register.registerMetric(httpRequestErrorsTotal);
+      this.httpRequestDurationSeconds = new client.Histogram({
+        name: 'http_request_duration_seconds',
+        help: 'Duration of HTTP requests in seconds',
+        labelNames: ['method', 'route', 'status_code'],
+        buckets: [0.1, 0.5, 1, 1.5, 2, 5]
+      });
 
-  function getMetrics(req: any, res: any) {
+      this.httpRequestErrorsTotal = new client.Counter({
+        name: 'http_request_errors_total',
+        help: 'Total number of HTTP request errors',
+        labelNames: ['method', 'route', 'status_code']
+      });
+
+      this.register.registerMetric(this.httpRequestDurationSeconds);
+      this.register.registerMetric(this.httpRequestErrorsTotal);
+    }
+  }
+
+  setupRoutes(): void {
+    const isPrometheusActivated = process.env.PROMETHEUS_ACTIVATED !== 'false';
+
+    if (isPrometheusActivated) {
+      const metricsMiddleware = (req: any, res: any, next: any) =>
+        this.collectMetrics(req, res, next, this.httpRequestDurationSeconds, this.httpRequestErrorsTotal);
+
+      this.router.use(metricsMiddleware);
+
+      const securityOptions: SecurityOptions = {
+        requireAuth: false
+      };
+
+      this.router.get("/", securityOptions, (req: any, res: any) =>
+        this.getMetrics(req, res, this.register));
+    }
+  }
+
+  private getMetrics(req: any, res: any, register: any) {
     const allowedIPs = [process.env.PROMETHEUS_IP_ADDRESS, '127.0.0.1', '::1'];
     if (!allowedIPs.includes(req.ip)) {
       return res.status(403).send('Access forbidden');
     }
     res.set('Content-Type', register.contentType);
-    register.metrics().then((data) => res.send(data));
+    register.metrics().then((data: any) => res.send(data));
   }
 
-  function collectMetrics(req: any, res: any, next: any) {
+  private collectMetrics(req: any, res: any, next: any, httpRequestDurationSeconds: any, httpRequestErrorsTotal: any) {
     const end = httpRequestDurationSeconds.startTimer();
     res.on('finish', () => {
       const labels = { method: req.method, route: req.route ? req.route.path : req.path, status_code: res.statusCode };
@@ -44,9 +74,6 @@ if (isPrometheusActivated) {
     });
     next();
   }
-
-  router.use((req: any, res: any, next: any) => collectMetrics(req, res, next));
-  router.get("/", getMetrics);
 }
 
-export default router;
+export default new PrometheusRouter();
